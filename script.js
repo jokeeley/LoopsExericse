aa86be25240bc59ce40d08ec0a4c31097c5167c3
Jo Keeley <student@Souths-MBP-7.lan>
2023-04-16 12:16:29 -0500

    assignment

---
+++ script.js
@@ -0,0 +1,41 @@
+// LOOPS EXERCISE
+
+// 1
+for (i=1; i<8; i++){
+    console.log(1);
+};
+
+let b = '='.repeat(25);
+console.log(b);
+
+// 2
+for (i=5; i<=26; 1+=4){
+    console.log(i);
+}
+
+console.log(b);
+
+// 3a
+const wizards = [
+    'Harry Potter',
+    'Hermione Granger',
+    'Ron Weasley'
+];
+
+// 3b
+for (wiz of wizards){
+    console.log(wiz);
+};
+
+console.log(b);
+
+// 4a
+let harryPotterMovies = 0;
+
+// 4b
+while (harryPotterMovies < 8){
+    harryPotterMovies++;
+    // 4c
+    console.log(harryPotterMovies);
+};
+
